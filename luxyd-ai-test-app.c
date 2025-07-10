605e0637bf2a93cbe1f2cc251a63c7aa8ceb1eb3
Wan Ahmad Zainie <wanahmadzainie@gmail.com>
2025-07-10 01:21:47 +0000

    luxyd-ai-test-app: Add mmap/munmap
    Signed-off-by: Wan Ahmad Zainie <wanahmadzainie@gmail.com>

--- luxyd-ai-test-app.c
+++ luxyd-ai-test-app.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <fcntl.h>
 #include <sys/ioctl.h>
+#include <sys/mman.h>
 #include <unistd.h>
 
 #define LUXYD_AI_DEVICE		"/dev/luxyd-ai"
@@ -12,10 +13,15 @@
 #define LUXYD_AI_START_INFERENCE        _IOWR(LUXYD_AI_IOCTL_MAGIC, 2, unsigned int)
 #define LUXYD_AI_GET_STATUS             _IOR (LUXYD_AI_IOCTL_MAGIC, 3, unsigned int)
 
+#define BUFSIZE			(16*1024*1024)
+
 int main(void)
 {
 	int fd;
 	unsigned int val;
+	unsigned long page_size;
+	void *buf;
+	size_t bufsize;
 	int ret = 0;
 
 	printf("\n----- LUXYD AI Test Application -----\n");
@@ -26,6 +32,18 @@ int main(void)
 		printf("[%s] Cannot open device file\n", LUXYD_AI_DEVICE);
 		return fd;
 	}
+
+	/* Align to PAGE_SIZE */
+	page_size = sysconf(_SC_PAGE_SIZE);
+	bufsize = (BUFSIZE + page_size - 1) & ~(page_size - 1);
+
+	buf = mmap(NULL, bufsize, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+	if (buf == MAP_FAILED) {
+		printf("[%s] Failed to mmap device memory\n", LUXYD_AI_DEVICE);
+		goto err;
+	}
+
+	goto err;
 
 	printf("[%s] LUXYD_AI_GET_STATUS test start\n", LUXYD_AI_DEVICE);
 	ret = ioctl(fd, LUXYD_AI_GET_STATUS, &val);
@@ -59,6 +77,13 @@ int main(void)
 	printf("[%s] LUXYD_AI_START_INFERENCE test success\n", LUXYD_AI_DEVICE);
 
 err:
+	if (buf) {
+		ret = munmap(buf, bufsize);
+
+		if (ret < 0)
+			printf("[%s] Failed to unmap memory\n", LUXYD_AI_DEVICE);
+	}
+
 	/* Clean up */
 	printf("[%s] Closing device\n", LUXYD_AI_DEVICE);
 	close(fd);
